778f131c3206923f20a8f0db2b70459cc187b246
Leo <leolmq#gmail.com>
2016-03-01 16:01:38 +0800

    camera fellow tanks
    camera fellow tanks

---
+++ Assets/Sprites/FollowTarget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour 
+{
+	public Transform play1;
+	public Transform play2;
+
+	private Vector3 offset;
+	private Camera camera;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - (play1.position + play2.position) / 2;
+		camera = this.GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = (play1.position + play2.position) / 2 + offset;
+		float distance = Vector3.Distance (play1.position, play2.position);
+		float size = distance * 0.58f;
+		camera.orthographicSize = size;
+	}
+}
