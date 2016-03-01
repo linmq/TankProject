0a5759cfcc029c51ae15f6b11aa1071a8f341ddb
Leo <leolmq#gmail.com>
2016-03-01 14:10:12 +0800

    add bullet for tank
    add bullet for tank

---
+++ Assets/TankAttack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankAttack : MonoBehaviour 
+{
+	public GameObject shellPrefab;
+	public KeyCode fireKey = KeyCode.Space;
+
+	private Transform firePosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		firePosition = transform.Find ("FirePosition");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (fireKey)) 
+		{
+			GameObject.Instantiate (shellPrefab, firePosition.position, firePosition.rotation);
+		}
+	}
+}
