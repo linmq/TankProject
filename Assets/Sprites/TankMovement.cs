8df5d4848d3fc6c15b092fd82464df9672e1391a
Leo <leolmq#gmail.com>
2016-03-01 11:45:10 +0800

    move tank
    move tank

---
+++ Assets/Sprites/TankMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMovement : MonoBehaviour
+{
+    public float speed = 5f;
+	public float angularSpeed = 10f;
+
+    private Rigidbody rigidbody;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+		rigidbody.velocity = transform.forward * v * speed;
+        
+		float h = Input.GetAxis ("Horizontal");
+		rigidbody.angularVelocity = transform.up * h * angularSpeed;
+    }
+}
