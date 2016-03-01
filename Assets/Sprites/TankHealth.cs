6cfd6794ecc6c845dd01e8b3b96c8c25b7555e64
Leo <leolmq#gmail.com>
2016-03-01 18:00:02 +0800

    add blood
    add blood

--- Assets/Sprites/TankHealth.cs
+++ Assets/Sprites/TankHealth.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TankHealth : MonoBehaviour 
 {
 	public int hp = 100;
 	public GameObject tankExplosion;
 	public AudioClip tankExplosionAudio;
+	public Slider hpSlider;
+
+	private int hpTotal;
+
+	void Start()
+	{
+		hpTotal = hp;
+	}
 
 	void TakeDamage()
 	{
@@ -14,6 +23,7 @@ public class TankHealth : MonoBehaviour
 			return;
 		}
 		hp -= Random.Range (10, 20);
+		hpSlider.value = (float)hp / hpTotal;
 		if (hp <= 0)
 		{
 			AudioSource.PlayClipAtPoint (tankExplosionAudio, Camera.main.gameObject.transform.position);
